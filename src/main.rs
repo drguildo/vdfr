3a3792c4018013f1040f168002e5ffcac4823726
Simon Morgan <sjm@sjm.io>
2021-11-04 19:16:11 +0000

    Print out app IDs in CLI

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,8 @@ use std::{fs, io::Cursor};
 fn main() {
     let appinfo_bytes = fs::read("appinfo.vdf").expect("Failed to read appinfo.vdf");
     let mut cursor = Cursor::new(appinfo_bytes);
-    vdfr::AppInfo::load(&mut cursor);
+    let appinfo = vdfr::AppInfo::load(&mut cursor);
+    for (app_id, _) in &appinfo.apps {
+        println!("{}", app_id);
+    }
 }
