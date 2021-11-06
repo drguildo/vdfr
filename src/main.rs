979a89511f80d29312288edac9eeee76158b9756
Simon Morgan <sjm@sjm.io>
2021-11-06 13:01:00 +0000

    Read files using a buffered reader

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
-use std::{fs, io::Cursor};
+use std::{fs, io::BufReader};
 
 fn main() {
-    let mut packageinfo_file = std::fs::File::open("packageinfo.vdf").expect("Failed to read packageinfo.vdf");
-    let packageinfo = vdfr::PackageInfo::load(&mut packageinfo_file);
-    for (package_id, _) in &packageinfo.packages {
-        println!("{}", package_id);
-    }
+    read_appinfo();
 }
 
 fn read_appinfo() {
-    let appinfo_bytes = fs::read("appinfo.vdf").expect("Failed to read appinfo.vdf");
-    let mut cursor = Cursor::new(appinfo_bytes);
-    let appinfo = vdfr::AppInfo::load(&mut cursor);
+    let mut appinfo_file =
+        BufReader::new(fs::File::open("appinfo.vdf").expect("Failed to read appinfo.vdf"));
+    let appinfo = vdfr::AppInfo::load(&mut appinfo_file);
     for (app_id, _) in &appinfo.apps {
         println!("{}", app_id);
+    }
+}
+
+fn read_packageinfo() {
+    let mut packageinfo_file =
+        BufReader::new(fs::File::open("packageinfo.vdf").expect("Failed to read packageinfo.vdf"));
+    let packageinfo = vdfr::PackageInfo::load(&mut packageinfo_file);
+    for (package_id, _) in &packageinfo.packages {
+        println!("{}", package_id);
     }
 }
